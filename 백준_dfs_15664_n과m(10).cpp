2a0d7440028dc83e6aa1043b0201d217e31d51be
kys1001 <kys_1001@naver.com>
2020-08-03 20:43:13 +0900

    0803

---
+++ 백준_dfs_15664_n과m(10).cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+vector<int> ans;
+vector<int> num;
+int check[10001];
+int cnt[20][10001];
+int n, m;
+void dfs(int idx){
+	if (ans.size() == m){
+		for (auto x : ans)
+			cout << x << " ";
+		cout << "\n";
+		return;
+	}
+	for (int  i =idx; i < num.size(); i++){
+		if (!check[i] && !cnt[ans.size()][num[i]]){
+			check[i] = 1;
+			cnt[ans.size()][num[i]] = 1;
+			ans.push_back(num[i]);
+			dfs(i);
+			ans.pop_back();
+			check[i] = 0;
+		}
+	}
+	memset(cnt[ans.size()], 0, sizeof(cnt[ans.size()]));
+}
+
+int main(){
+	cin >> n >> m;
+	for (int i = 0; i < n; i++){
+		int tmp;
+		cin >> tmp;
+		num.push_back(tmp);
+	}
+	sort(num.begin(), num.end());
+	dfs(0);
+}
