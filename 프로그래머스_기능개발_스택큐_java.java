b3312c0ae7b0f9164303d17720896f221ca05508
kys1001 <kys_1001@naver.com>
2020-09-08 01:22:59 +0900

    0907

--- 프로그래머스_기능개발_스택큐_java.java
+++ 프로그래머스_기능개발_스택큐_java.java
@@ -5,42 +5,55 @@ package algo;
 
 import java.util.*;
 
-class Pair{
-	String key;
-	int value;
-	Pair(String key, int value){
-		this.key = key;
-		this.value = value;
-	}
-}
 public class Main{
 	class Solution {
-		public static int[] solution(String[] genres, int[] plays) {
-            int [] answer= {};
-			HashMap<String, Integer> mp = new HashMap<>();
-			ArrayList<Pair> list = new ArrayList<>();
-			
-			for(int i=0;i<genres.length;i++) {
-				if(!mp.containsKey(genres[i])) {
-					list.add(new Pair(genres[i],0));
-					mp.put(genres[i], plays[i]);
-				}
-				else {
-					mp.put(genres[i],mp.get(genres[i])+plays[i]);
-				}
-			}
-			for(int i=0;i<mp.size();i++) {
-				list.get(i).value = mp.get(list.get(i));
-			}
-			for(int i=0;i<list.size();i++) {
-				System.out.print(list.get(i).key + " " +list.get(i).value);
-			}
-			return answer;
-		}
-    }
-	public static void main(String[] args) {
-		String[] genres = {"classic","pop","classic","classic","pop"};
-		int[] plays = {500,600,150,800,2500};
-		Solution.solution(genres,plays);
+	    public int[] solution(int[] progresses, int[] speeds) {
+	    	int[] answer = {};
+	    	ArrayList<Integer> list = new ArrayList<>();
+	    	for(int i=0;i<progresses.length;i++) {
+	    		if((100-progresses[i]) % speeds[i] != 0) {
+	    			list.add((100-progresses[i])/speeds[i]+1);
+	    		}
+	    		else{
+	    			list.add((100-progresses[i])/speeds[i]);
+	    		}
+	    	}
+	    	ArrayList <Integer> tmp_list = new ArrayList<>();
+	    	for(int i=0;i<list.size();i++) {
+	    		tmp_list.add(list.get(i));
+	    	}
+	    	int []check = new int[tmp_list.size()];
+	    	ArrayList <Integer> tmp_answer = new ArrayList<>();
+	    	Collections.sort(tmp_list);
+
+	    	for(int i=0;i<tmp_list.size();i++) {
+	    		int time = tmp_list.get(i);
+	    		int count =0;
+	    		for(int j=0;j<list.size();j++) {
+	    			if(time >= list.get(j)) {
+	    				if(check[j] == 0) {
+	    					check[j]= 1;
+	    					count++;
+	    				}
+	    			}
+	    			else {
+	    				if(check[j] == 1) {
+	    					continue;
+	    				}
+	    				else {
+	    					break;
+	    				}
+	    			}
+	    		}
+	    		if(count != 0)
+	    			tmp_answer.add(count);
+	    	}
+	    	answer = new int[tmp_answer.size()];
+	    	for(int i=0;i<tmp_answer.size();i++) {
+	    		answer[i] = tmp_answer.get(i);
+	    	}
+	    	
+	    	return answer;
+	    }
 	}
 }
